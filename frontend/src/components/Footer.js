494d1aab278543659d01479d602a769efa493cc0
Asfak-Ahamed <asfak18102004@gmail.com>
2026-08-22 22:19:52 +0530

    Add  ecommerce project

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return  <footer className="py-1 bg-dark">
                 <p className="text-center text-white mt-1 ">
-                    JVLcart - 2023-2024, All Rights Reserved
+                   ASH_CART 2026, All Rights Reserved
                 </p>
                 <p className="text-center text-white mt-1 ">
                    
