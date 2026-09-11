de2e9947aaaf69de7044d15a2b23caa46849db30
Asfak-Ahamed <asfak18102004@gmail.com>
2026-09-11 18:26:16 +0530

    Fix React Router deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <App /> 
+  <BrowserRouter basename="/E-Commerce">
+    <App />
+</BrowserRouter>
   </React.StrictMode>
 );
 
